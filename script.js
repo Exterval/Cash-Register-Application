7c1bebb514f4c70b6dbfd1a76c298efb2bed911e
Exterval <treblmao@gmail.com>
2025-09-13 23:54:47 +0800

    added a bit of js and tweaks in design

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 // CASH REGISTER APP
+
+const purchageBtn = document.getElementById('purchase-btn');
+const change = document.getElementById('change-due');
+const cash = document.getElementById('cash');
